8dd19b6888848afc356830cec064080b167f03a1
KwaHash <cosmosray001@gmail.com>
2025-07-30 20:53:56 +0900

    feat: pattern-manager page

---
+++ app/pattern-manager/page.tsx
@@ -0,0 +1,5 @@
+import PatternManagerPage from '@/features/PatternManagerpage'
+
+export default function PatternManager() {
+  return <PatternManagerPage />
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function splitFormat(format: string) {
+  return format.split(/,|\n/).map(word => word.replace(/\s+/g, '').trim()).filter(word => word !== '')
+}
+
+export function joinFormat(format: string[]) {
+  return format.join(", ");
+}
